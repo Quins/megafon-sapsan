8fd3b966a6c1da7105355473f5f7e6c28d54c1ca
verbaux <verbaux@yandex.ru>
2014-11-28 19:07:39 +0400

    start project
    - add header
    - add not-money
    - add time-expired (need to add cross in circle)

---
+++ README.md
@@ -0,0 +1,6 @@
+Blanq – пустой проект для harp
+=====
+
+Инициализация: 
+
+    harp init projectname --boilerplate Quins/blanq
---
+++ public/js/common.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+
+	
+});
+
+
+/* Unsorted */
+function executeFunction(name, context) {
+	
+	var context = context ? context : window;
+	var properties = Array.prototype.slice.call(arguments).splice(2, 100);
+	var namespaces = name.split(".");
+	var func = namespaces.pop();
+	
+	for(var i = 0; i < namespaces.length; i++) {
+		
+		context = context[namespaces[i]];
+	}
+	
+	return context[func].apply(this, properties);
+}
+
+function getElementPercentageWidth(element) {
+	
+	var width = element.width();
+	var parentWidth = element.offsetParent().width();
+	
+	return Math.ceil(100 * (width / parentWidth));
+}
+
+function getSubstring(string, substringPattern) {
+	
+	var searchResults = string.match(substringPattern);
+	
+	return ((searchResults && searchResults[1]) ? searchResults[1] : "");
+}
+
+var identificators = {};
+
+function generateIdentificator() {
+
+	var identificator = '';
+	var identificatorLength = 10;
+	var charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	var charsetLength = charset.length;
+
+	for (i = 0; identificatorLength > i; i += 1) {
+  
+		var charIndex = Math.random() * charsetLength;  
+		identificator += charset.charAt(charIndex);  
+	}
+	
+	identificator = identificator.toLowerCase();
+
+	if (identificators[identificator])
+		return generateIdentificator();
+
+	identificators[identificator] = true;  
+
+	return identificator;
+}
+
+var cookiesDomain = "quins.ru";
+
+function createCookie(name, value, days) {
+
+	if (days) {
+		
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		var expires = "; expires=" + date.toGMTString();
+
+	} else
+		var expires = "";
+	
+	document.cookie = name + "=" + value + expires + "; path=/" + ((cookiesDomain) ? "; domain=" + cookiesDomain : "");
+}
+
+function readCookie(name) {
+	
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+
+	for(var i = 0; i < ca.length; i++) {
+
+		var c = ca[i];
+		
+		while (c.charAt(0) == ' ')
+			c = c.substring(1,c.length);
+		
+		if (c.indexOf(nameEQ) == 0)
+			return c.substring(nameEQ.length,c.length);
+	}
+
+	return "";
+}
+
+function eraseCookie(name) {
+
+	createCookie(name, "", -1);
+}
